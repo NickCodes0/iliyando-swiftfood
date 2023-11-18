80eb7bc50a053d8dcbe87ae05fdab7c20d1e9707
NickCodes0 <113610395+NickCodes0@users.noreply.github.com>
2023-11-18 11:22:06 +0000

    Content Update

---
+++ assets/php/sites/user/food_list.php
@@ -0,0 +1,48 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iliyando SwiftFood | Essen</title>
+</head>
+
+<div class="row">
+    <div class="col-md-6">
+        <div class="card dark-mode-bg" style="width: 18rem;">
+            <img src="https://www.el-gusto.net/wp-content/uploads/2021/04/Pain-au-Chocolat-Title-930x620.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Schokobrötchen</h5>
+                <p class="card-text">Ein Schokobrötchen vom Bäcker.</p>
+                <a href="#" class="btn btn-info card-box-button">Bestellen</a><span class="card-price" style="padding-left: 32%;">0,70€</span>
+            </div>
+        </div>
+        <br>
+    </div>
+    <div class="col-md-6">
+        <div class="card dark-mode-bg" style="width: 18rem;">
+            <img src="https://img.chefkoch-cdn.de/rezepte/2490741391600404/bilder/1506553/crop-960x720/broetchen-perfekt-wie-vom-baecker.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Brötchen</h5>
+                <p class="card-text">Ein Brötchen vom Bäcker.</p>
+                <a href="#" class="btn btn-info card-box-button">Bestellen</a><span class="card-price" style="padding-left: 32%;">0,30€</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
+
+<div class="row">
+    <div class="col-md-6">
+        <div class="card dark-mode-bg" style="width: 18rem;">
+            <img src="https://www.baeckerei-middelberg.de/fileadmin/_processed_/4/9/csm_Der_Middelberger_Mehrkornbroetchen_Ei_0edd813172.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Belegtes Brötchen</h5>
+                <p class="card-text">Ein belegtes Brötchen vom Bäcker.</p>
+                <a href="#" class="btn btn-info card-box-button">Bestellen</a><span class="card-price" style="padding-left: 32%;">0,30€</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    console.log('Essen - Uhrzeit', new Date().toLocaleTimeString());
+</script>
